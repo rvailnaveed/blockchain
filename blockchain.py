2c0efe95dc19f227067a985cb26673033479774d
Rvail Naveed <naveedm@tcd.ie>
2020-01-15 19:27:44 +0000

    proof of work algorithm, set up Flask

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,9 @@
 import hashlib
 import json
+from textwrap import dedent
 from time import time
+from uuid import uuid4
+from flask import Flask
 
 class Blockchain(object):
 
@@ -50,6 +53,20 @@ class Blockchain(object):
         return self.last_block['index'] + 1
 
     """
+        Proof of Work Algorithm:
+         - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+         - p is the previous proof, and p' is the new proof
+        :param last_proof: <int>
+        :return: <int>
+    """
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    """
         Creates a SHA-256 hash of a Block
         :param block: <dict> Block
         :return: <str>
@@ -57,10 +74,52 @@ class Blockchain(object):
     @staticmethod
     def hash(block):
         block_string = json.dumps(block, sort_keys=True).encode()
-        
+
         return hashlib.sha256(block_string).hexdigest()
+
+    """
+        Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not.
+    """
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+
+        return guess_hash[:4] == "0000"
 
     # Returns the last block in the chain
     @property
     def last_block(self):
         return self.chain[-1]
+
+    # Instantiate Node
+    app = Flask(__name__)
+
+    # Generate a globally unique address for this node
+    node_id = str(uuid4()).replace('-', '')
+
+    # Instantiate Blockchain
+    blockchain = Blockchain()
+
+    @app.route('/mine', methods=['GET'])
+    def mine():
+        pass
+
+    @app.route('/transactions/new', methods=['POST'])
+    def new_transaction():
+        pass
+
+    @app.route('/chain', methods=['GET'])
+    def full_chain():
+        response = {
+            'chain': blockchain.chain,
+            'length': len(blockchain.chain)
+        }
+        return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
